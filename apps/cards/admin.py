7d0837db7a105f1373e9f8dcef4d4a260f221628
luis-rios <vvrhz11@gmail.com>
2020-12-11 20:28:51 -0600

    'model_card'

--- apps/cards/admin.py
+++ apps/cards/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.cards.models import Card
+
+admin.site.register(Card)
--- apps/cards/models.py
+++ apps/cards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Card(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(default=timezone.now)
+    expiration_date = models.DateTimeField(default=timezone.now)
+    position = models.IntegerField(default=None)
+    members = models.ManyToManyField(User, related_name='card')
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
