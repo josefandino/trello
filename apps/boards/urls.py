a773d00d8bb4b2223660068b3c3a65a3a994a3ec
shecho <shechovala@gmail.com>
2020-12-13 21:06:23 -0500

    boards endpoints

--- apps/boards/urls.py
+++ apps/boards/urls.py
@@ -3,6 +3,5 @@ from rest_framework.routers import DefaultRouter
 from .views import BoardViewSet
 
 router = DefaultRouter()
-router.register (r'tablero', BoardViewSet)
+router.register(r'boards', BoardViewSet)
 urlpatterns = router.urls
-
