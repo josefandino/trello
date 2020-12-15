f7406fecf66a2f9801ec8117ef07210b4c1e4811
motokovil <65578314+motokovil@users.noreply.github.com>
2020-12-15 15:53:18 -0500

    pull dev

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,17 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-from .views import UserViewSet
+from .views import UserViewSet, RegisterView, LoginAPIView
 
 router = DefaultRouter()
 
 router.register(r'', UserViewSet)
 
-urlpatterns = router.urls
+
+
+urlpatterns = [
+    path('register/',RegisterView.as_view(),name= "register"),
+    path('login/', LoginAPIView.as_view(), name="login"),
+]
+
+urlpatterns += router.urls
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from rest_framework.decorators import action
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, RegisterSerializer, EmailVerificationSerializer, LoginSerializer
 from ..boards.serializers import BoardSerializer
 from rest_framework.response import Response
+from rest_framework.response import  Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -19,3 +21,25 @@ class UserViewSet(viewsets.ModelViewSet):
         if request.method == 'GET':
             serializer = BoardSerializer(user.board, many=True)
             return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self,request):
+        
+        user = request.data
+        serializer = self.serializer_class(data = user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+
+        return Response({"message": "checked email",**user_data},status=status.HTTP_201_CREATED)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK) 
