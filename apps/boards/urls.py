f2ab7af3918c6783e4b06e439e55bae72b28e2ae
luis-rios <vvrhz11@gmail.com>
2020-12-14 22:00:40 -0600

    'arreglando_card_view'

--- apps/boards/urls.py
+++ apps/boards/urls.py
@@ -3,5 +3,5 @@ from rest_framework.routers import DefaultRouter
 from .views import BoardViewSet
 
 router = DefaultRouter()
-router.register(r'', BoardViewSet)
+router.register(r'viewset', BoardViewSet)
 urlpatterns = router.urls
--- apps/cards/serializers.py
+++ apps/cards/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-
-from apps.cards.models import Card
+from .models import Card
 
 
 class CardSerializer(serializers.ModelSerializer):
