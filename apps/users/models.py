234f626cf341cb3dd016a5b359615901f4ebeead
shecho <shechovala@gmail.com>
2020-12-15 20:46:42 -0500

    some cjhahgnes

--- apps/users/models.py
+++ apps/users/models.py
@@ -36,7 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     lastname = models.CharField(max_length=120, blank=False, null=False)
     is_verified = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
