7c0ac9968502723913d041ef793acde1176f758e
dixroby <alestilodefe@gmail.com>
2020-12-17 14:51:46 -0500

    Fix: Se corrige la importacion de path

--- apps/boards/urls.py
+++ apps/boards/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
-
 from .views import BoardViewSet,BoardListUser
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'', BoardViewSet)
