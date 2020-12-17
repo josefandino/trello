11fc85bc34d2264bc0a5bcb5d5dd329e5b64b403
dixroby <alestilodefe@gmail.com>
2020-12-17 15:28:57 -0500

    Refactor: Modelo user

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import User
 
 
-# from django.contrib import auth
-# from rest_framework.exceptions import AuthenticationFailed
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,59 +11,59 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
-# class RegisterSerializer(serializers.ModelSerializer):
-#
-#     password = serializers.CharField(max_length = 100,min_length=4,write_only=True)
-#
-#     class Meta:
-#         model = User
-#         #fields = '__all__'
-#         fields = ['email','username','password']
-#
-#     def validate(self, attrs):
-#         email = attrs.get('email', '')
-#         username = attrs.get('username', '')
-#
-#         if not username.isalnum():
-#             raise serializers.ValidationError(
-#                 'The username should only contain alphanumeric characters')
-#         return attrs
-#
-#     def create(self, validated_data):
-#         return User.objects.create_user(**validated_data)
-#
-# class EmailVerificationSerializer(serializers.ModelSerializer):
-#
-#     token = serializers.CharField(max_length=555)
-#     class Meta:
-#         model = User
-#         fields = ['token']
-#
-# class LoginSerializer(serializers.ModelSerializer):
-#     email = serializers.EmailField(max_length=255,min_length=3)
-#     password = serializers.CharField(max_length=255,min_length=1,write_only=True)
-#     username = serializers.CharField(max_length=255,min_length=1,read_only=True)
-#     tokens = serializers.CharField(max_length=255,min_length=1,read_only=True)
-#
-#     class Meta:
-#         model = User
-#         fields=['email','password','username','tokens']
-#     def validate(self, attrs):
-#         email=attrs.get('email','')
-#         password = attrs.get('password', '')
-#         user = auth.authenticate(email=email,password=password)
-#         """ import pdb
-#         pdb.set_trace() """
-#         if not user :
-#             raise AuthenticationFailed('Invalid credentials, try again')
-#         if not user.is_active :
-#             raise AuthenticationFailed('Account disabled, contact admin')
-#         if not user.is_verified:
-#             raise AuthenticationFailed('Email is not verified')
-#
-#         return {
-#             'email':user.email,
-#             'username':user.username,
-#             'tokens':user.tokens
-#         }
-#         return super().validate(attrs)
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(max_length = 100,min_length=4,write_only=True)
+
+    class Meta:
+        model = User
+        #fields = '__all__'
+        fields = ['email','username','password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError(
+                'The username should only contain alphanumeric characters')
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+
+    token = serializers.CharField(max_length=555)
+    class Meta:
+        model = User
+        fields = ['token']
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255,min_length=3)
+    password = serializers.CharField(max_length=255,min_length=1,write_only=True)
+    username = serializers.CharField(max_length=255,min_length=1,read_only=True)
+    tokens = serializers.CharField(max_length=255,min_length=1,read_only=True)
+
+    class Meta:
+        model = User
+        fields=['email','password','username','tokens']
+    def validate(self, attrs):
+        email=attrs.get('email','')
+        password = attrs.get('password', '')
+        user = auth.authenticate(email=email,password=password)
+        """ import pdb
+        pdb.set_trace() """
+        if not user :
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active :
+            raise AuthenticationFailed('Account disabled, contact admin')
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+
+        return {
+            'email':user.email,
+            'username':user.username,
+            'tokens':user.tokens
+        }
+        return super().validate(attrs)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,46 +1,41 @@
 from django.shortcuts import render
-
-from rest_framework import viewsets, status, generics
+from ..boards.serializers import BoardSerializer
+from .models import User
+from .serializers import UserSerializer, RegisterSerializer, EmailVerificationSerializer, LoginSerializer
+from rest_framework import viewsets, status, generics, mixins
 from rest_framework.decorators import action
 
-from .models import User
-from .serializers import UserSerializer
-# from .serializers import UserSerializer, RegisterSerializer, EmailVerificationSerializer, LoginSerializer
-from ..boards.serializers import BoardSerializer
-# from rest_framework.response import Response
 from rest_framework.response import Response
-# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
+# class UserViewSet(mixins.RetrieveModelMixin,
+#                    mixins.UpdateModelMixin,
+#                    mixins.DestroyModelMixin,
+#                    mixins.ListModelMixin,
+#                    GenericViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @action(methods=['GET', 'POST', 'DELETE'], detail=True)
-    def boards(self, request, pk=None):
-        user = self.get_object()
-        if request.method == 'GET':
-            serializer = BoardSerializer(user.board, many=True)
-            return Response(status=status.HTTP_200_OK, data=serializer.data)
+class RegisterView(generics.GenericAPIView):
 
-# class RegisterView(generics.GenericAPIView):
-#
-#      serializer_class = RegisterSerializer
-#
-#      def post(self,request):
-#
-#          user = request.data
-#          serializer = self.serializer_class(data = user)
-#          serializer.is_valid(raise_exception=True)
-#          serializer.save()
-#          user_data = serializer.data
-#
-#          return Response({"message": "checked email",**user_data},status=status.HTTP_201_CREATED)
+     serializer_class = RegisterSerializer
 
-# class LoginAPIView(generics.GenericAPIView):
-#      serializer_class = LoginSerializer
-#
-#      def post(self, request):
-#          serializer = self.serializer_class(data=request.data)
-#          serializer.is_valid(raise_exception=True)
-#          return Response(serializer.data, status=status.HTTP_200_OK)
+     def post(self,request):
+
+         user = request.data
+         serializer = self.serializer_class(data = user)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         user_data = serializer.data
+
+         return Response({"message": "checked email",**user_data},status=status.HTTP_201_CREATED)
+
+class LoginAPIView(generics.GenericAPIView):
+     serializer_class = LoginSerializer
+
+     def post(self, request):
+         serializer = self.serializer_class(data=request.data)
+         serializer.is_valid(raise_exception=True)
+         return Response(serializer.data, status=status.HTTP_200_OK)
